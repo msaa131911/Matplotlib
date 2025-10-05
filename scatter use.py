635ca6b197a5873e9003d039035472c29233edd3
MSAA <msaa131911@gmail.com>
2025-10-05 19:27:18 +0600

    Update scatter use.py

--- scatter use.py
+++ scatter use.py
@@ -6,5 +6,6 @@ size=np.arange(8,13)
 
 price=np.arange(300,550,50)
 
-plt.scatter(size,price) #scatter plot use
-plt.show()
+plt.scatter(size,price) #scatter plot use 
+
+plt.show() # show() ar pori borta plt.scatter(size,price) ; korta pari
