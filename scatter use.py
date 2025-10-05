b2ccae9fac63e6f0043e8280b550a7c463892dc5
MSAA <msaa131911@gmail.com>
2025-10-05 19:23:18 +0600

    Add files via upload

---
+++ scatter use.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+size=np.arange(8,13)
+
+price=np.arange(300,550,50)
+
+plt.scatter(size,price) #scatter plot use
+plt.show()
